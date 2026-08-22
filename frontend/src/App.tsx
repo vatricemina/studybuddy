8601827a02e1da98753c036ee32b674a5547f9b9
vatricemina <evatric1@etf.unsa.ba>
2026-08-22 02:42:22 +0200

    add protected route component to protect dashboard from unauthenticated access

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,14 +7,21 @@ import {Routes, Route, Navigate} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import RegisterPage from './pages/RegisterPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
       <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />}/>
           <Route path="/register" element={<RegisterPage />} />
+          <Route
+              path="/dashboard"
+              element={
+                  <ProtectedRoute>
+                      <DashboardPage />
+                  </ProtectedRoute>}
+          />
       </Routes>
   );
 }
---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import { Navigate } from 'react-router-dom';
+import type { ReactNode } from 'react';
+
+function ProtectedRoute({ children }: {children: ReactNode}){
+    const token=localStorage.getItem("token");
+    if(!token){
+        return <Navigate to="/login"/>
+    }
+
+    return <>{children}</>
+}
+
+export default ProtectedRoute;
